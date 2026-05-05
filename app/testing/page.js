331d2f683b8f6725fdb9dd301f66c1e7ba8a2304
haris386 <harisahmed873@gmail.com>
2026-05-05 18:07:40 +0500

    T9

---
+++ app/testing/page.js
@@ -0,0 +1,80 @@
+"use client";
+import { useEffect, useRef, useState } from "react";
+import Footer from "@/components/Footer";
+import { Button } from "@/components/ui/button";
+import BuiltForStructuredExecution from "@/components/BuiltForStructuredExecution";
+import UnstructuredDocumentationCostsMoney from "@/components/UnstructuredDocumentationCostsMoney";
+import WeStructureProof from "@/components/WeStructureProof";
+import GetStartedModal from "@/components/GetStartedModal";
+import HeroLayout from "@/components/HeroLayout";
+import { DesktopUpdatedHeader } from "@/components/DesktopUpdatedHeader";
+
+const content = (
+  <p className="text-blue-100">
+    AccuraCore is a powerful platform that brings everything you need into one
+    dashboard, manage your team, track work orders, handle finances, and stay in
+    control of every detail.
+  </p>
+);
+
+const buttons = (onGetStarted) => (
+  <>
+    <Button
+      className="rounded-full text-sm flex items-center"
+      style={{
+        backgroundColor: "#fff",
+        color: "#0061A4",
+        padding: "27px 15px",
+      }}
+      onClick={onGetStarted}
+    >
+      Get Started
+      <img src="/Icons/Vector2.png" alt="Arrow Icon" className="w-2 h-3 ml-2" />
+    </Button>
+
+    <Button
+      className="text-white rounded-full text-sm flex items-center"
+      style={{
+        backgroundColor: "#0061A4",
+        border: "2px solid #4485b2",
+        padding: "25px 15px",
+      }}
+      onClick={() => (window.location.href = "/book-a-demo")}
+    >
+      Book Demo
+      <img src="/Icons/Vector.png" alt="Arrow Icon" className="w-2 h-3 ml-2" />
+    </Button>
+  </>
+);
+
+
+export default function TestingPage() {
+  const [isOpen, setIsOpen] = useState(false);
+  const videoRefElement = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  return (
+    <>
+      {/* DESKTOP HERO AND HEADER */}
+  <HeroLayout
+          logo="/Logos/Accuracam/AccuraCam-Logo.png"
+          content={content}
+          buttons={buttons(() => setIsOpen(true))}
+        />
+
+      {/* <HeroLayout
+        logo="/Logos/Accuracam/AccuraCam-Logo.png"
+        content={content}
+        buttons={buttons(() => setIsOpen(true))}
+      /> */}
+
+      <BuiltForStructuredExecution isOpen={isOpen} setIsOpen={setIsOpen} />
+      <UnstructuredDocumentationCostsMoney />
+      <WeStructureProof isOpen={isOpen} setIsOpen={setIsOpen} />
+
+      <Footer id="contact-section" onBookDemo={() => setIsOpen(true)} />
+      {/* Popup Modal */}
+      {isOpen && <GetStartedModal isOpen={isOpen} setIsOpen={setIsOpen} />}
+    </>
+  );
+}
